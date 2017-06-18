36146077863d77889e8ad456017b7dae0edfb8f8
Eric <eric@gmail.com>
2017-06-18 08:12:38 +0800

    [ADD] define a function called iteOrder

--- hello.c
+++ hello.c
@@ -1,5 +1,31 @@
 #include "stdio.h"
+void iteOrder();
 int main(void) {
-	printf("Hello World\n");
+	iteOrder();
 	return 0;
+}
+
+void iteOrder() {
+	int i = 10;
+	while(i--) {
+		if(i==0) continue;
+		switch(i) {
+			case 1: {
+				printf("this is the %dst time\n",i);
+			}
+			break;
+			case 2: {
+			    printf("this is the %dnd time\n",i);
+			}
+			break;
+			case 3: {
+			    printf("this is the %drd time\n",i);
+			}
+			break;
+			default: {
+                printf("this is the %dth time\n",i);
+			}
+			break;
+		}
+	}
 }
