de59a6a716d7db4f675ebbf778f30b542856596e
Eric <eric@gmail.com>
2017-06-23 11:56:23 +0800

    [ADD] Add a new directory to list knowledge about protocol

---
+++ network/packetSwitching.js
@@ -0,0 +1,23 @@
+/**
+ * message: 报文
+ * packet: 分组
+ * header: 头部控制信息
+ * data: 数据区
+ */
+const message = {
+    packet1: {
+        header: {
+            sourceIp: '',//源Ip
+            targetIp: ''//目標ip
+        },
+        data: {}
+    },
+    packet2: {
+        header: {},
+        data: {}
+    },
+    packetN: {
+        header: {},
+        data: {}
+    }
+};
